aa9a33cd95074c8f2d9d0a3687dc55e0970b6d5f
Yogi Prasetya Sadewa <yogiprasetya907@gmail.com>
2026-08-22 13:17:33 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -14,8 +14,11 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "prefer-const": "off",
     },
   },
 ];
